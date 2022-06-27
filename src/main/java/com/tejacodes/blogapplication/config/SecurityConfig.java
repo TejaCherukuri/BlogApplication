a5f026701cf3ffd5962f68d2262a153700cca6a3
TejaCherukuri <tejkrish98@gmail.com>
2022-06-27 17:45:12 +0530

    Added Role Based Authentication using UserDetails from the Database

--- src/main/java/com/tejacodes/blogapplication/config/SecurityConfig.java
+++ src/main/java/com/tejacodes/blogapplication/config/SecurityConfig.java
@@ -1,18 +1,18 @@
 package com.tejacodes.blogapplication.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import com.tejacodes.blogapplication.security.CustomerUserDetailsService;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -20,6 +20,8 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableGlobalMethodSecurity(prePostEnabled = true)		// Used to enable @Pre and @Post annotations used for controlling role based access on apis.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private CustomerUserDetailsService customerUserDetailsService;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -33,24 +35,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.httpBasic();
 	}
 	
+	// This method is used to configure/define roles in-memory i.e: gets deleted upon server restart/stop
+	
+//	@Override
+//	@Bean
+//	protected UserDetailsService userDetailsService() {
+//		
+//		UserDetails user = User.builder()
+//								.username("teja") 
+//								.password(passwordEncoder().encode("password"))
+//								.roles("USER")
+//								.build();
+//		
+//		UserDetails admin = User.builder()
+//								.username("admin")
+//								.password(passwordEncoder().encode("admin"))
+//								.roles("ADMIN")
+//								.build();
+//		
+//		return new InMemoryUserDetailsManager(user,admin);
+//	}
+	
+	
+	// This method is used to configure role based authentication using user details from the database
 	@Override
-	@Bean
-	protected UserDetailsService userDetailsService() {
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		UserDetails user = User.builder()
-								.username("teja") 
-								.password(passwordEncoder().encode("password"))
-								.roles("USER")
-								.build();
+		auth.userDetailsService(customerUserDetailsService)
+			.passwordEncoder(passwordEncoder());
 		
-		UserDetails admin = User.builder()
-								.username("admin")
-								.password(passwordEncoder().encode("admin"))
-								.roles("ADMIN")
-								.build();
-		
-		return new InMemoryUserDetailsManager(user,admin);
-	}
+		}
 	
 	@Bean
 	PasswordEncoder passwordEncoder()
---
+++ src/main/java/com/tejacodes/blogapplication/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.tejacodes.blogapplication.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tejacodes.blogapplication.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	Optional<User> findByEmail(String email);
+	
+	Optional<User> findByUsername(String username);
+	
+	Optional<User> findByUsernameOrEmail(String username, String email);
+	
+	Boolean existsByUsername(String username);
+	
+	Boolean existsByEmail(String email);
+}
